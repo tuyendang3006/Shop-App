bc632dd7cac815234de9dad40ecc0325e2638ccf
tuyendang3006 <jesminedang@gmail.com>
2020-06-16 01:28:12 +0200

    First Shop App

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $199",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "$200 to $499",
+        "array": [200, 499]
+    },
+    {
+        "_id": 3,
+        "name": "$500 to $749",
+        "array": [500, 749]
+    },
+    {
+        "_id": 4,
+        "name": "$750 to $999",
+        "array": [750, 999]
+    },
+    {
+        "_id": 5,
+        "name": "More than $1000",
+        "array": [1000, 1500000]
+    }
+]
+
+
+
+const categories = [
+    {
+        "_id": 1,
+        "name": "Laptop"
+    },
+    {
+        "_id": 2,
+        "name": "Tablet"
+    },
+    {
+        "_id": 3,
+        "name": "Telephone"
+    },
+    {
+        "_id": 4,
+        "name": "Speaker"
+    },
+    {
+        "_id": 5,
+        "name": "Headphones"
+    },
+    {
+        "_id": 6,
+        "name": "Watch"
+    },
+    {
+        "_id": 7,
+        "name": "Accessories"
+    }
+]
+
+
+export {
+    price,
+    categories
+}
